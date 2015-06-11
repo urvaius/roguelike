cccd91479c9b84f60bf52994523180928489f21f
Dave Arneson <urvaius@gmail.com>
2015-06-11 16:09:58 -0700

    onto lesson 10

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,18 +6,24 @@ public class GameManager : MonoBehaviour
 
 	public static GameManager instance = null;
 	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
+
+
+
 	private int level = 3;
 
 	// Use this for initialization
 	void Awake ()
 	{
 
-        if (instance == null)
-            instance = this;
-        else if (instance != null)
-            Destroy(gameObject);
+		if (instance == null)
+			instance = this;
+		else if (instance != null)
+			Destroy(gameObject);
 
-        DontDestroyOnLoad(gameObject);
+		DontDestroyOnLoad(gameObject);
 		boardScript = GetComponent<BoardManager>();
 		InitGame();
 	
@@ -28,6 +34,11 @@ public class GameManager : MonoBehaviour
 	{
 		boardScript.SetupScene(level);
 	}
+
+    public void GameOver()
+    {
+        enabled = false;
+    }
 	// Update is called once per frame
 	void Update ()
 	{
